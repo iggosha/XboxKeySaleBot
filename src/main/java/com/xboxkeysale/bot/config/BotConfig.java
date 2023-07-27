a97470c5d2e0ad3620d4f6b6f9e9eb256df144a2
iggosha <iegolovkov@gmail.com>
2023-07-28 00:08:45 +0300

    Revert "Удалён неиспользуемый класс, заменённый Environment + проведён рефакторинг"
    This reverts commit 8b35369024d2a8ecaf531ae49bc6a05be470c41f.

---
+++ src/main/java/com/xboxkeysale/bot/config/BotConfig.java
@@ -0,0 +1,21 @@
+package com.xboxkeysale.bot.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@NoArgsConstructor
+@PropertySource("/application.properties")
+public class BotConfig {
+
+    @Value("${bot.username}")
+    String name;
+    @Value("${bot.token}")
+    String token;
+    @Value("${bot.chatId}")
+    String chatId;
+}
